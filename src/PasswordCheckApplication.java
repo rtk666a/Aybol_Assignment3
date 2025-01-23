634f33204e87ed7788179761744ee0762fc42917
RTK <126328596+rtk666a@users.noreply.github.com>
2025-01-23 13:57:28 +0600

    update

--- src/PasswordCheckApplication.java
+++ src/PasswordCheckApplication.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class PasswordCheckApplication {
     public static void main(String[] args) {
             Layout layout = new Layout();
             System.out.println(layout.layout());
